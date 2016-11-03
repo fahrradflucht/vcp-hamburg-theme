110c636c0cd1ec436009502a83e4a5f086802805
Fahrradflucht <mail@mathiswiehl.de>
2016-11-03 10:15:41 +0100

    Add page.php
    Progress on #2

---
+++ page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+<?php get_header(); ?>
+<main class="main">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <div class="news">
+            <h1 class="news-heading"><?php the_title(); ?>:</h1>
+            <div class="news-posts">
+                <div class="news-post">
+                    <div class="news-post-container">
+                        <p class="entry"><?php the_content(); ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endwhile; endif; ?>
+</main>
+<?php get_footer(); ?>
+<?php wp_footer(); ?>
+</body>
+</html>
