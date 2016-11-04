38c81deaca38077c069c92ca345df48fad77060f
Fahrradflucht <mail@mathiswiehl.de>
2016-11-04 17:14:58 +0100

    Add Turbolinks
    We use cdnjs for that. This means users have to pull resources from 3 different external sources now. I think it will still be faster then serving this stuff our self.

--- footer.php
+++ footer.php
@@ -6,4 +6,5 @@
         &middot
         <a href=<?php echo get_page_by_title('kontakt')->guid; ?> class="footer-content-link">Kontakt</a>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/turbolinks/5.0.0/turbolinks.min.js" integrity="sha256-HUtkNsGc3BlCNQTNPUr5JsjW0wQFeU4nfYOEuUcxptU=" crossorigin="anonymous"></script>
 </setion>
