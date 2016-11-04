4926ce178622b8e2cec2ad9f496fc49447aefc7d
Fahrradflucht <mail@mathiswiehl.de>
2016-11-04 20:18:14 +0100

    Display child pages as tabs
    Closes #2

--- functions.php
+++ functions.php
@@ -1,6 +1,40 @@
 <?php
 
-function register_my_menu() {
+function has_parent($id) {
+  return !empty(get_post_ancestors($id));
+}
+
+function get_parent_id($id) {
+  return get_post_ancestors($id)[0];
+}
+
+function page_menu_items($id) {
+  if(has_parent($id)){
+    $id = get_parent_id($id);
+  }
+
+  $my_wp_query = new WP_Query();
+  $all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));
+
+  return get_page_children( $id, $all_wp_pages );
+}
+
+function is_current_uri($uri) {
+  global $wp;
+  $current = add_query_arg( $wp->query_string, '', trailingslashit(home_url( $wp->request )));
+  return $uri == $current;
+}
+
+function get_parent_title($id) {
+  if(!has_parent($id)){
+    return get_the_title();
+  }
+  
+  return get_page(get_parent_id($id))->post_title;
+}
+
+function register_navigation() {
   register_nav_menu('navigation',__( 'Navigation' ));
 }
-add_action( 'init', 'register_my_menu' );
+
+add_action( 'init', 'register_navigation' );
--- page.php
+++ page.php
@@ -4,10 +4,22 @@
 <main class="main">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <div class="news">
-            <h1 class="news-heading"><?php the_title(); ?>:</h1>
+            <h1 class="news-heading"><?php echo get_parent_title(get_the_ID()); ?>:</h1>
             <div class="news-posts">
                 <div class="news-post">
                     <div class="news-post-container">
+                        <div class="page-nav">
+                            <?php
+                            foreach (page_menu_items(get_the_ID()) as $child) {
+                                if(is_current_uri($child->guid)) {
+                                    echo '<a class="page-nav-item page-nav-item_current" href="'.$child->guid.'">'.$child->post_title.'</a>';
+                                } else {
+                                    echo '<a class="page-nav-item" href="'.$child->guid.'">'.$child->post_title.'</a>';
+                                }
+                            }
+                            ?>
+                            <?php has_parent(get_the_ID()); ?>
+                        </div>
                         <p class="entry"><?php the_content(); ?></p>
                     </div>
                 </div>
