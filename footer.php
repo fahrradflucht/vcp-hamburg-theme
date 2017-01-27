339ee846dc490335d0ff2a26f2397c9fc0744c0c
Fahrradflucht <mail@mathiswiehl.de>
2017-01-27 20:21:33 +0100

    Disable turbolinks caching

--- footer.php
+++ footer.php
@@ -7,5 +7,4 @@
         <a href=<?php echo get_page_by_title('kontakt')->guid; ?> class="footer-content-link">Kontakt</a>
     </div>
 </setion>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/turbolinks/5.0.0/turbolinks.min.js" integrity="sha256-HUtkNsGc3BlCNQTNPUr5JsjW0wQFeU4nfYOEuUcxptU=" crossorigin="anonymous"></script>
 <?php wp_footer(); ?>
