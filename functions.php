fahrradflucht/vcp-hamburg-theme

commit 45d69e59037932f2d4bb8c48a2f2ab3fead5d421

fix: srcset string generation on real urls
Only replace the last dot in the URL not everyone.

functions.php
@@ -41,9 +41,15 @@ function get_parent_title($id) {
  */
 function get_srcset($img, $sizes) {
     $img_path = get_bloginfo('template_directory') . $img;
+    $last_dot = strrpos($img_path, '.');
     return join(', ', array_map(
         function($size) use ($img_path) {
-            return str_replace('.', '-' . $size . '.', $img_path . ' ' . $size . 'w');
+            return substr_replace(
+                '.',
+                '-' . $size . '.', $img_path . ' ' . $size . 'w',
+                $last_dot,
+                strlen($img_path)
+            );
         },
         $sizes
     ));

commit 75af5448d9d25ed61500245c34d59afabbc3ea46

fix: srcset generation
fix bug that got introduced in 45d69e59037932f2d4bb8c48a2f2ab3fead5d421

functions.php
@@ -43,12 +43,12 @@ function get_srcset($img, $sizes) {
     $img_path = get_bloginfo('template_directory') . $img;
     $last_dot = strrpos($img_path, '.');
     return join(', ', array_map(
-        function($size) use ($img_path) {
+        function($size) use ($img_path, $last_dot) {
             return substr_replace(
-                '.',
-                '-' . $size . '.', $img_path . ' ' . $size . 'w',
+                $img_path . ' ' . $size . 'w',
+                '-' . $size . '.',
                 $last_dot,
-                strlen($img_path)
+                1
             );
         },
         $sizes