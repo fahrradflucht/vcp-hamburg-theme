b2fc08b797bf9eb4afa57c680ba192f899e182e4
Mathis Wiehl <mathis.wiehl@jimdo.com>
2019-05-14 20:46:42 +0200

    fix: use permalinks for footers

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <section class="footer">
     <div class="footer-content">
-        <a href=<?php echo get_page_by_title('datenschutzerklärung')->guid; ?> class="footer-content-link">Datenschutzerkl&auml;rung</a>
+        <a href=<?php echo get_permalink(get_page_by_title('datenschutzerklärung')); ?> class="footer-content-link">Datenschutzerkl&auml;rung</a>
         &middot
-        <a href=<?php echo get_page_by_title('impressum')->guid; ?> class="footer-content-link">Impressum</a>
+        <a href=<?php echo get_permalink(get_page_by_title('impressum')); ?> class="footer-content-link">Impressum</a>
         &middot
         <a href="<?php echo site_url(); ?>" class="footer-content-link">VCP Hamburg</a>
         &middot
-        <a href=<?php echo get_page_by_title('kontakt')->guid; ?> class="footer-content-link">Kontakt</a>
+        <a href=<?php echo get_permalink(get_page_by_title('kontakt')); ?> class="footer-content-link">Kontakt</a>
     </div>
 </section>
 <?php wp_footer(); ?>
